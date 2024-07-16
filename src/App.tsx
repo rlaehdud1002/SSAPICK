fd98a7a3916010e99a0f08ecbbdd7c7064cc4e59
김도영 <rlaehdud1002@naver.com>
2024-07-16 22:00:43 +0900

    feat: navbar route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Ranking from './pages/Ranking';
+import Pick from './pages/Pick';
+import Message from './pages/Message';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className="flex flex-col relative">
-      <Header />
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/ranking" element={<Ranking />} />
+            <Route path="/pick" element={<Pick />} />
+            <Route path="/message" element={<Message />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
