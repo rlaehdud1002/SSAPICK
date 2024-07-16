8e2131703a8d97500bebebabfb42e992fc957f85
김도영 <rlaehdud1002@naver.com>
2024-07-16 14:29:49 +0900

    pwa 아이콘 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className="luckiest_guy logo_font_color">SSAP</h1>
     </>
   );
 }
