dd650125347ffea40fad9671fe556a5f64801a85
이호영 <hoo4053@naver.com>
2024-07-18 14:17:47 +0900

    shadcn 세팅

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -1,4 +1,4 @@
-import TrophyIcon from "../icons/TrophyIcon";
+import TrophyIcon from "icons/TrophyIcon";
 
 interface RankingProps {
   isRanked: boolean;
@@ -14,23 +14,23 @@ const Ranking = ({isRanked}:RankingProps) => {
         <TrophyIcon width={25} height={25} />
         <h2>가장 많이 Pick한 질문</h2>
       </div>
-      <div className="w-52 h-8 border-2 border-white rounded-md " >1. {}</div>
-      <div className="w-52 h-8 border-2 border-white rounded-md " >2. {}</div>
-      <div className="w-52 h-8 border-2 border-white rounded-md" >3. {}</div>
+      <div  >1.</div>
+      <div  >2.</div>
+      <div  >3.</div>
       <div className="flex">
         <TrophyIcon width={25} height={25} />
         <h2>가장 많이 스킵된 질문</h2>
       </div>
-      <div className="w-52 h-8 border-2 border-white rounded-md " >1. {}</div>
-      <div className="w-52 h-8 border-2 border-white rounded-md " >2. {}</div>
-      <div className="w-52 h-8 border-2 border-white rounded-md " >3. {}</div>
+      <div  >1.</div>
+      <div  >2.</div>
+      <div  >3.</div>
       <div className="flex">
         <TrophyIcon width={25} height={25} />
         <h2>가장 많이 지목된 사람</h2>
       </div>
-      <div className="w-52 h-8 border-2 border-white rounded-md " >1. {}</div>
-      <div className="w-52 h-8 border-2 border-white rounded-md " >2. {}</div>
-      <div className="w-52 h-8 border-2 border-white rounded-md " >3. {}</div>
+      <div  >1.</div>
+      <div  >2.</div>
+      <div  >3.</div>
       </div>
       :
       <div> 랭킹을 위한 정보가 부족합니다.</div>
