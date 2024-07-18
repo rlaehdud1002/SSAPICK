af7d3c10be2b593080ac11d40e16ded0b099c6c8
이호영 <hoo4053@naver.com>
2024-07-18 16:50:24 +0900

    [CONFLICT] 리액트 폼으로 마이그레이션

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import Header from './components/common/Header';
 import Footer from './components/common/Footer';
-import Login from './pages/Login';
-import UserInfo from './pages/UserInfo';
-import UserAddInfo from './pages/UserAddInfo';
+import Header from './components/common/Header';
 import Home from './pages/Home';
-import Ranking from './pages/Ranking';
-import Pick from './pages/Pick';
 import Message from './pages/Message';
+import Pick from './pages/Pick';
 import Profile from './pages/Profile';
+import Ranking from './pages/Ranking';
+import UserInfo from './pages/UserInfo';
 
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="flex flex-col relative">
       <Header />
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/ranking" element={<Ranking isRanked={true} />} />
-            <Route path="/pick" element={<Pick />} />
-            <Route path="/message" element={<Message />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </div>
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/ranking" element={<Ranking isRanked={true} />} />
+          <Route path="/pick" element={<Pick />} />
+          <Route path="/message" element={<Message />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
       {/* <Login/> */}
-      <UserInfo/>
+      <UserInfo />
       {/* <UserAddInfo/> */}
       <div className="flex flex-col min-h-screen">
         <Footer />
