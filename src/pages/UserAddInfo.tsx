057f41d185434164cfc92f1a771fe94121e2f1bc
이호영 <leehoyoung@ihoyeong-ui-MacBookAir.local>
2024-07-21 00:59:50 +0900

    [FEAT] todo추가정보입력페이지 ui 수정

--- src/pages/UserAddInfo.tsx
+++ src/pages/UserAddInfo.tsx
@@ -23,15 +23,16 @@ const UserAddInfo = () => {
     }
 
     return <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div className="flex flex-col my-5">
         <h3 style={{ fontSize: 13 }}>추가 정보 입력</h3>
         <div style={{ fontSize: 13 }}>추후 힌트로 사용됩니다.</div>
-        <div style={{ color: "red", fontSize: 10 }}>최소 2개의 정보 입력이 필수입니다.</div>
-        <div className="flex w-full flex-col justify-center items-center mx-10 space-y-2">
+        <div className="mb-20" style={{ color: "red", fontSize: 10 }}>최소 2개의 정보 입력이 필수입니다.</div>
+        <div className="flex w-full flex-col justify-center items-center space-y-2">
 
             <InfoInput title="MBTI" register={register("mbti", {
                 required: "MBTI를 입력해주세요.",
                 maxLength: { value: 4, message: "4글자로 입력해주세요." },
-                minLength: { value: 4, message: "4글자로 입력해주세요." }
+                minLength: { value: 4, message: "4글자로 입력해주세요." },
             })} />
             <InfoInput title="전공" register={register("major", {
                 required: "전공을 입력해주세요.",
@@ -48,9 +49,10 @@ const UserAddInfo = () => {
                 required: "관심사를 입력해주세요.",
                 maxLength: { value: 20, message: "20글자 이하로 입력해주세요." }
             })} />
-
-            
+            <div>
             <DoneButton title="완료" />
+            </div>
+            </div>
         </div>
     </form>
 
