4813df9ec40969ee09d736f83c014c33930b0101
이호영 <leehoyoung@ihoyeong-ui-MacBookAir.local>
2024-07-21 15:08:55 +0900

    [FEAT] 폴더 정리

--- src/pages/MattermostDone.tsx
+++ src/pages/MattermostDone.tsx
@@ -4,7 +4,7 @@ const MattermostDone = () => {
   return (
     <div className="flex flex-col items-center">
         <div className="flex items-center my-40">
-        <img className="w-10 h-10" src="../icons/check.png" alt="" />
+        <img className="w-10 h-10" src="../icons/Check.png" alt="" />
         <h1>인증 완료</h1>
         </div>
         <DoneButton title="튜토리얼 보기"/>
