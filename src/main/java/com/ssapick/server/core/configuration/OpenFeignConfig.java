c2830c94b40dcfce350f29d2cfcd307733b1c984
JaewonChoi <wefhio1985@gmail.com>
2024-07-22 09:34:27 +0900

    [FEAT] ãOpenFeign config í 클래스 추가

--- src/main/java/com/ssapick/server/core/configuration/OpenFeignConfig.java
+++ src/main/java/com/ssapick/server/core/configuration/OpenFeignConfig.java
@@ -1,4 +1,4 @@
-package org.example.openfeigndemo;
+package com.ssapick.server.core.configuration;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
