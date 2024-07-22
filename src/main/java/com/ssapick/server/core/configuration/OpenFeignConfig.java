ee80578f299beefe086a1e6113aff3253a7d742c
JaewonChoi <wefhio1985@gmail.com>
2024-07-22 10:16:24 +0900

    [FEAT] Mattermost 인증 데이터 받 받아오기

--- src/main/java/com/ssapick/server/core/configuration/OpenFeignConfig.java
+++ src/main/java/com/ssapick/server/core/configuration/OpenFeignConfig.java
@@ -4,6 +4,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients("org.example.openfeigndemo")
+@EnableFeignClients("com.ssapick.server.domain.auth.controller")
 public class OpenFeignConfig {
 }
