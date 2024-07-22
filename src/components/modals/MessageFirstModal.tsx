b74fd4252bddd2c88fac0633ade54feb55d7d43e
김도영 <rlaehdud1002@naver.com>
2024-07-22 10:22:14 +0900

    [FEAT] form 에러 메시지 출력

--- src/components/modals/MessageFirstModal.tsx
+++ src/components/modals/MessageFirstModal.tsx
@@ -4,6 +4,9 @@ import UserIcon from 'icons/UserIcon';
 import { Button } from 'components/ui/button';
 import { Input } from 'components/ui/input';
 
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+
 import {
   Dialog,
   DialogContent,
@@ -21,12 +24,32 @@ interface MessageFirstModalProps {
   onCreate: () => void;
 }
 
+interface MessageForm {
+  message: string;
+}
+
 const MessageFirstModal = ({
   show,
   onOpen,
   onClose,
   onCreate,
 }: MessageFirstModalProps) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<MessageForm>();
+
+  const onSubmit = (data: MessageForm) => {
+    console.log('ok', data);
+    onCreate();
+    onClose();
+  };
+
+  const onInvalid = (errors: any) => {
+    console.log('errors', errors);
+  };
+
   return (
     <Dialog open={show} onOpenChange={(isOpen) => !isOpen && onClose()}>
       <DialogTrigger onClick={onOpen}>
@@ -40,20 +63,32 @@ const MessageFirstModal = ({
               <UserIcon />
               <h3 className="ms-3">11기 2반</h3>
             </div>
-            {/* <Input
-              className="input-box border-none h-10 focus:outline-none"
+            <Input
               type="text"
-            /> */}
+              className="input-box border-none h-10 focus:outline-none"
+              register={register('message', {
+                required: '쪽지 내용을 입력해주세요.',
+                maxLength: {
+                  value: 100,
+                  message: '100글자 이하로 입력해주세요.',
+                },
+              })}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="message"
+              render={({ message }) => (
+                <h6 className="text-red-500 text-left text-xs">{message}</h6>
+              )}
+            />
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="text-right">
           <Button
+            type="submit"
             variant="ssapick"
             size="lg"
-            onClick={() => {
-              onCreate();
-              onClose();
-            }}
+            onClick={handleSubmit(onSubmit, onInvalid)}
           >
             <CoinIcon width={25} height={25} />
             <h3 className="luckiest_guy ms-2 me-4">1</h3>전송
