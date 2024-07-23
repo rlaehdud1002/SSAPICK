f7174e3aec4afc8ce1d2933bd485a619fa90b1a9
김도영 <rlaehdud1002@naver.com>
2024-07-23 10:24:51 +0900

    [FEAT] 알림 컴포넌트, 뒤로 가기

---
+++ src/components/AlarmPage/AlarmContent.tsx
@@ -0,0 +1,40 @@
+import LocationAlarmIcon from 'icons/Alarm/LocationAlarmIcon';
+import MessageAlarmIcon from 'icons/Alarm/MessageAlarmIcon';
+import PickAlarmIcon from 'icons/Alarm/PickAlarmIcon';
+import QuestionAlarmIcon from 'icons/Alarm/QuestionAlarmIcon';
+
+interface AlarmContentProps {
+  category: string;
+  content: string;
+  read: boolean;
+}
+
+const AlarmContent = ({ category, content, read }: AlarmContentProps) => {
+  return (
+    <div className="bg-white/50 rounded-md flex flex-row p-3">
+      <div>
+        {category === 'location' && (
+          <LocationAlarmIcon width={42} height={42} />
+        )}
+        {category === 'message' && <MessageAlarmIcon width={42} height={42} />}
+        {category === 'pick' && <PickAlarmIcon width={42} height={42} />}
+        {category === 'question' && (
+          <QuestionAlarmIcon width={42} height={42} />
+        )}
+      </div>
+      <div className="flex flex-col ms-3">
+        <p>{content}</p>
+        <div className="flex flex-row">
+          <p className="text-xs text-gray-500">1분 전</p>
+          {read && (
+            <p className="text-xs text-red-500 luckiest_guy bg-white/50 rounded-full px-1 ms-1">
+              N
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlarmContent;
--- src/pages/Alarm.tsx
+++ src/pages/Alarm.tsx
@@ -1,22 +1,26 @@
-import LocationAlarmIcon from 'icons/Alarm/LocationAlarmIcon';
-import MessageAlarmIcon from 'icons/Alarm/MessageAlarmIcon';
-import PickAlarmIcon from 'icons/Alarm/PickAlarmIcon';
-import QuestionAlarmIcon from 'icons/Alarm/QuestionAlarmIcon';
 import AlarmIcon from 'icons/AlarmIcon';
 import BackIcon from 'icons/BackIcon';
+import AlarmContent from 'components/AlarmPage/AlarmContent';
+
+import { useNavigate } from 'react-router-dom';
 
 const Alarm = () => {
+  const nav = useNavigate();
+
   return (
     <div>
-      <div className="flex flex-row items-center m-2">
+      <div className="flex flex-row items-center m-2 cursor-pointer" onClick={() => nav(-1)}>
         <BackIcon />
         <AlarmIcon className="me-2" />
         <h1>알림</h1>
       </div>
-      <LocationAlarmIcon width={25} height={25} />
-      <MessageAlarmIcon width={25} height={25} />
-      <PickAlarmIcon width={25} height={25} />
-      <QuestionAlarmIcon width={25} height={25} />
+      <div className="m-6">
+        <AlarmContent
+          category="location"
+          content="XXX님이 반경 Xm 안으로 들어왔습니다."
+          read
+        />
+      </div>
     </div>
   );
 };
