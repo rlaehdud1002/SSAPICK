ad15c3746fdb757ad6dbdfc0b5e67dfa9b4e0c98
김도영 <rlaehdud1002@naver.com>
2024-07-23 11:58:07 +0900

    [FEAT] ranking page component

--- client/src/pages/Ranking.tsx
+++ client/src/pages/Ranking.tsx
@@ -1,27 +1,26 @@
-import TrophyIcon from "../icons/TrophyIcon";
+import RankContent from 'components/RankingPage/RankContent';
 
-interface RankingProps {
-  isRanked: boolean;
-}
+const Ranking = () => {
+  const fakeList = [
+    {
+      rank: 1,
+      content: '질문 1',
+    },
+    {
+      rank: 2,
+      content: '질문 2',
+    },
+    {
+      rank: 3,
+      content: '질문 3',
+    },
+  ];
 
-const Ranking = ({isRanked}:RankingProps) => {
   return (
-      isRanked ? 
-    <div>
-
-      <h1>Ranking Page</h1> 
-      <div className="flex">
-        <TrophyIcon width={25} height={25} />
-        <h2>가장 많이 Pick한 질문</h2>
-      </div>
-      <div  >1.</div>
-      <div  >2.</div>
-      <div  >3.</div>
-      </div>
-      :
-      <div> 랭킹을 위한 정보가 부족합니다.</div>
-
-    
+    <div className="m-6">
+      <RankContent title="가장 많이 대답한 질문?" rankList={fakeList} />
+      <RankContent title="가장 많이 대답한 질문?" rankList={[]} />
+    </div>
   );
 };
 
