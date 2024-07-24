8ac6ed6f9904bc0da8ffc00e51e702af49f49339
김도영 <rlaehdud1002@naver.com>
2024-07-24 13:46:39 +0900

    [FEAT] message page icon 추가

--- client/src/pages/Routes/CommonRoute.tsx
+++ client/src/pages/Routes/CommonRoute.tsx
@@ -1,3 +1,5 @@
+import Received from 'components/MessagePage/Received';
+import Send from 'components/MessagePage/Send';
 import Alarm from 'pages/Alarm';
 import Home from 'pages/Home';
 import Message from 'pages/Message';
@@ -6,17 +8,21 @@ import Profile from 'pages/Profile';
 import Ranking from 'pages/Ranking';
 import { Route, Routes } from 'react-router-dom';
 
-const CommonRoute = ()=>{
+const CommonRoute = () => {
   return (
     <Routes>
-       <Route path="/" element={<Home />} />
-              <Route path="/alarm" element={<Alarm />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/ranking" element={<Ranking/>} />
-              <Route path="/pick" element={<Pick />} />
-              <Route path="/message" element={<Message />} />
-              <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/alarm" element={<Alarm />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/ranking" element={<Ranking />} />
+      <Route path="/pick" element={<Pick />} />
+      <Route path="/message" element={<Message />}>
+        <Route path="" element={<Received />} />
+        <Route path="received" element={<Received />} />
+        <Route path="send" element={<Send />} />
+      </Route>
+      <Route path="/profile" element={<Profile />} />
     </Routes>
-  )
-}
+  );
+};
 export default CommonRoute;
