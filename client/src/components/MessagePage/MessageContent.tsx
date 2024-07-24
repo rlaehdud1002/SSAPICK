2267d40366bd14ca19ac73f2134b5b7f384fc58f
김도영 <rlaehdud1002@naver.com>
2024-07-24 13:47:37 +0900

    [FEAT] message page icon 추가

--- client/src/components/MessagePage/MessageContent.tsx
+++ client/src/components/MessagePage/MessageContent.tsx
@@ -1,5 +1,6 @@
 import MessageQuestionIcon from 'icons/MessageQuestionIcon';
 import UserPickIcon from 'icons/UserPickIcon';
+import PointIcon from 'icons/PointIcon';
 
 interface MessageContentProps {
   name: string;
@@ -18,9 +19,12 @@ const MessageContent = ({
 }: MessageContentProps) => {
   return (
     <div className="mx-2 my-5 border-b-[1px]">
-      <div className="flex flex-row items-center">
-        <UserPickIcon gen={gen} />
-        <h1 className="ms-3">{name}</h1>
+      <div className="flex flex-row justify-between">
+        <div className='flex flex-row items-center'>
+          <UserPickIcon gen={gen} />
+          <h1 className="ms-3">{name}</h1>
+        </div>
+        <PointIcon />
       </div>
       <div className="text-center text-gray-500 my-4">{question}</div>
       <div className="flex flex-row items-center bg-white/50 rounded-lg p-2">
