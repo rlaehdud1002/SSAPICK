73b737f760b104db6faaaf255f511e505bb78e1f
김도영 <rlaehdud1002@naver.com>
2024-07-25 20:30:04 +0900

    [FEAT] attendance

--- client/src/icons/AttendanceIcon.tsx
+++ client/src/icons/AttendanceIcon.tsx
@@ -1,14 +1,29 @@
 interface AttendanceIconProps {
   width: number;
   height: number;
+  setpage?: boolean;
 }
 
-const AttendanceIcon = ({width, height}:AttendanceIconProps) => {
-  return <img
-  width={width}
-  height={height}
-  src="icons/Attend.png" 
-  alt="attendance" />
-}
+const AttendanceIcon = ({ width, height, setpage }: AttendanceIconProps) => {
+  return (
+    <>
+      {setpage ? (
+        <img
+          width={width}
+          height={height}
+          src="icons/Attendance.png"
+          alt="attendance"
+        />
+      ) : (
+        <img
+          width={width}
+          height={height}
+          src="icons/Attend.png"
+          alt="attendance"
+        />
+      )}
+    </>
+  );
+};
 
 export default AttendanceIcon;
