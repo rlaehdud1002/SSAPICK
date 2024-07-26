11e553c3b8ff10535e8722d10bd2587bb1f21a57
김도영 <rlaehdud1002@naver.com>
2024-07-26 15:00:41 +0900

    [FEAT] 위치 알림 하는중

--- client/src/pages/LocationAlarm.tsx
+++ client/src/pages/LocationAlarm.tsx
@@ -1,5 +1,20 @@
+import BackIcon from 'icons/BackIcon';
+
+import { useNavigate } from 'react-router-dom';
+
 const LocationAlarm = () => {
-  return <div>LocationAlarm</div>
-}
+  const nav = useNavigate();
+  
+  return (
+    <div>
+      <div
+        className="flex flex-row items-center m-2 cursor-pointer"
+        onClick={() => nav(-1)}
+      >
+        <BackIcon />
+      </div>
+    </div>
+  );
+};
 
 export default LocationAlarm;
