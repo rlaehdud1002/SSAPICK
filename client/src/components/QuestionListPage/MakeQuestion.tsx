4bf04d5e5bf0502c5b5581752561003fa688bb77
이호영 <leehoyoung@ihoyeong-ui-MacBookAir.local>
2024-07-29 00:01:35 +0900

    [FEAT] 질문리스트 페이지 ui 완료

--- client/src/components/QuestionListPage/MakeQuestion.tsx
+++ client/src/components/QuestionListPage/MakeQuestion.tsx
@@ -1,5 +1,13 @@
+import MakeQuestionContent from "./MakeQuestionlContent"
+
 const MakeQuestion = () => {
-    return <div> 생성한 질문</div>
+    return (
+        <div className="mb-20">
+        {[0, 1, 2, 3, 4, 5, 6].map((index) => (
+            <MakeQuestionContent key={index} question="같이 밥먹고 싶은 사람은?"/>
+          ))}
+        </div>       
+)
 }
 
 export default MakeQuestion
--- client/src/components/QuestionListPage/QuestionInfo.tsx
+++ client/src/components/QuestionListPage/QuestionInfo.tsx
@@ -1,6 +1,35 @@
+import AlarmedQuestion from "./AlarmedQuestion";
+import QuestionInfoContent from "./RankedQuestion"
+
 const QuestionInfo = () => {
+    const fakeList = [
+        {
+          rank: 1,
+          content: '질문 1',
+        },
+        {
+          rank: 2,
+          content: '질문 2',
+        },
+        {
+          rank: 3,
+          content: '질문 3',
+        },
+      ];
     return (
-     <div>질문 리스트</div>   
+    <div className="ml-5">
+     <QuestionInfoContent title="내가 지목된 질문" rankList={fakeList}/>
+     {/* <Separator className="my-4 mx-4" />  */}
+    <div className="bg-white h-px w-90 mx-1 mt-5"></div>
+    <div className="mt-5">
+    <span>알림 설정한 질문</span>
+    <div className="mb-20">
+    {[0, 1, 2, 3, 4, 5,6,6].map((index) => (
+            <AlarmedQuestion key={index} gender="여자" title="같이 밥먹고 싶은 사람?"/>
+          ))}
+    </div>
+    </div>
+     </div>
     )
 }
 
