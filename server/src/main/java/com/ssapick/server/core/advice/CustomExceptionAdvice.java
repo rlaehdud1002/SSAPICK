ef962c3a351f64ff5a2f9eb62aa3c3aedbfa0904
JaewonChoi <wefhio1985@gmail.com>
2024-07-29 15:26:14 +0900

    [REFACT] 사용하지 않는 import 구문 삭제

--- server/src/main/java/com/ssapick/server/core/advice/CustomExceptionAdvice.java
+++ server/src/main/java/com/ssapick/server/core/advice/CustomExceptionAdvice.java
@@ -4,17 +4,12 @@ import com.ssapick.server.core.exception.BaseException;
 import com.ssapick.server.core.exception.ErrorCode;
 import com.ssapick.server.core.response.CustomValidationError;
 import com.ssapick.server.core.response.ErrorResponse;
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
