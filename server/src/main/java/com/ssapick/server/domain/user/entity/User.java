53a00ea8192bb18eae76fb1d40849cdcf4fab28e
INJUN <dldlswns890@naver.com>
2024-07-31 15:42:34 +0900

    [MERGE] User 병합 충돌 해결

--- server/src/main/java/com/ssapick/server/domain/user/entity/User.java
+++ server/src/main/java/com/ssapick/server/domain/user/entity/User.java
@@ -1,142 +1,110 @@
 package com.ssapick.server.domain.user.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.ssapick.server.core.entity.BaseEntity;
 import com.ssapick.server.domain.attendance.entity.Attendance;
 import com.ssapick.server.domain.pick.entity.Hint;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Table(
-        name = "users",
-        uniqueConstraints = {@UniqueConstraint(columnNames = "username")},
-        indexes = {
-                @Index(name = "index_user_username", columnList = "username")
-        }
+	name = "users",
+	uniqueConstraints = {@UniqueConstraint(columnNames = "username")},
+	indexes = {
+		@Index(name = "index_user_username", columnList = "username")
+	}
 )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class User extends BaseEntity {
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "followUser")
-    private final List<Follow> followers = new ArrayList<>();
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "followUser")
+	private final List<Follow> followers = new ArrayList<>();
 
-    @OneToMany(mappedBy = "toUser", cascade = CascadeType.ALL)
-    private final List<UserBan> bannedUser = new ArrayList<>();
+	@OneToMany(mappedBy = "toUser", cascade = CascadeType.ALL)
+	private final List<UserBan> bannedUser = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private final List<Hint> hints = new ArrayList<>();
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private final List<Hint> hints = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private final List<Attendance> attendances = new ArrayList<>();
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private final List<Attendance> attendances = new ArrayList<>();
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
 
 	@OneToOne(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private Profile profile;
+	private Profile profile;
 
 	@Column(nullable = false)
-    private String username;
+	private String username;
 
 	@Column(nullable = false)
-    private char gender;
+	private char gender;
 
 	@Column(nullable = false)
-    private String name;
+	private String name;
 
 	@Column(nullable = false)
-    private String email;
+	private String email;
 
 	@Column(name = "provider_type", nullable = false)
-    @Enumerated(EnumType.STRING)
+	@Enumerated(EnumType.STRING)
 	private ProviderType providerType;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType = RoleType.USER;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType = RoleType.USER;
 
-    @Column(name = "provider_id", nullable = false)
-    private String providerId;
+	@Column(name = "provider_id", nullable = false)
+	private String providerId;
 
-    @Column(name = "is_mattermost_confirmed", nullable = false)
-    private boolean isMattermostConfirmed = false;
+	@Column(name = "is_mattermost_confirmed", nullable = false)
+	private boolean isMattermostConfirmed = false;
 
-    @Column(name = "is_locked", nullable = false)
-    private boolean isLocked = false;
+	@Column(name = "is_locked", nullable = false)
+	private boolean isLocked = false;
 
-    /**
-     * 사용자 생성 메서드
-     *
-     * @param username     사용자 이름
-     * @param gender
-     * @param providerType 제공자 타입 (GOOGLE, NAVER, KAKAO)
-     * @param providerId   제공자 ID
-     * @return {@link User} 새롭게 생성한 유저 객체
-     */
-    public static User createUser(String username, String name, char gender, ProviderType providerType,
-                                  String providerId) {
-        User user = new User();
-        user.username = username;
-        user.name = name;
-        user.email = username;
-        user.gender = gender;
-        user.providerType = providerType;
-        user.providerId = providerId;
-        user.profile = Profile.createEmptyProfile(user);
-        return user;
-    }
-
-    public void mattermostConfirm() {
-        this.isMattermostConfirmed = true;
-    }
-
-
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Hint> hints = new ArrayList<>();
-
-    public void updateUser(String username, String name) {
-        this.username = username;
-        this.name = name;
-    }
-
-    public void updateName(String newName) {
-        this.name = newName;
-    }
-
-    public void updateGender(char newGender) {
-        this.gender = newGender;
-    }
-
-    public void updateProfile(Profile newProfile) {
-        this.profile = newProfile;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", profile=" + profile +
-                ", username='" + username + '\'' +
-                ", gender=" + gender +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", providerType=" + providerType +
-                '}';
-    }
-
-	public void mattermostConfirm() {
-		this.isMattermostConfirmed = true;
-	}
-
-	public void setTestId(Long id) {
-		this.id = id;
+	/**
+	 * 사용자 생성 메서드
+	 *
+	 * @param username     사용자 이름
+	 * @param gender
+	 * @param providerType 제공자 타입 (GOOGLE, NAVER, KAKAO)
+	 * @param providerId   제공자 ID
+	 * @return {@link User} 새롭게 생성한 유저 객체
+	 */
+	public static User createUser(String username, String name, char gender, ProviderType providerType,
+		String providerId) {
+		User user = new User();
+		user.username = username;
+		user.name = name;
+		user.email = username;
+		user.gender = gender;
+		user.providerType = providerType;
+		user.providerId = providerId;
+		user.profile = Profile.createEmptyProfile(user);
+		return user;
 	}
 
 	public void updateUser(String username, String name) {
@@ -156,25 +124,20 @@ public class User extends BaseEntity {
 		this.profile = newProfile;
 	}
 
+	public void mattermostConfirm() {
+		this.isMattermostConfirmed = true;
+	}
+
+	public void setTestId(Long id) {
+		this.id = id;
+	}
+
 	public void updateHints(List<Hint> hints) {
 		this.hints.clear();
 		this.hints.addAll(hints);
 		for (Hint hint : hints) {
 			hint.updateUser(this);
 		}
-	}
-
-	@Override
-	public String toString() {
-		return "User{" +
-			"id=" + id +
-			", profile=" + profile +
-			", username='" + username + '\'' +
-			", gender=" + gender +
-			", name='" + name + '\'' +
-			", email='" + email + '\'' +
-			", providerType=" + providerType +
-			'}';
 	}
 
 	public void delete() {
