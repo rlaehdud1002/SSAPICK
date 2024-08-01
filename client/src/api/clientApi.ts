c44b7fec125842b67ec31376509c1ee0a1768ae9
INJUN <dldlswns890@naver.com>
2024-08-01 17:57:17 +0900

    [FEAT] 로그아웃 API

--- client/src/api/clientApi.ts
+++ client/src/api/clientApi.ts
@@ -1,6 +1,6 @@
-import { accessTokenState } from 'atoms/UserAtoms';
-import axios from 'axios';
-import { getRecoil } from 'recoil-nexus';
+import { accessTokenState } from "atoms/UserAtoms";
+import axios from "axios";
+import { getRecoil } from "recoil-nexus";
 
 const BASE_URL = process.env.REACT_APP_BACKEND_PROD_HOST;
 
@@ -8,10 +8,10 @@ export const KAKAO_AUTH_URL = `${BASE_URL}/oauth2/authorization/kakao`;
 export const GOOGLE_AUTH_URL = `${BASE_URL}/oauth2/authorization/google`;
 
 const instance = axios.create({
-  baseURL: BASE_URL + '/api/v1',
+  baseURL: BASE_URL + "/api/v1",
   headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
+    "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
 
@@ -26,7 +26,7 @@ instance.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  },
+  }
 );
 
 instance.interceptors.response.use(
@@ -35,7 +35,17 @@ instance.interceptors.response.use(
   },
   (error) => {
     return Promise.reject(error);
-  },
+  }
 );
 
 export default instance;
+
+export const SignOut = async () => {
+  try {
+    await instance.post("auth/sign-out");
+    // 여기에 로그아웃 성공시 처리를 넣어주세요.
+    window.location.href = "/";
+  } catch (error) {
+    console.error("로그아웃 실패:", error);
+  }
+};
