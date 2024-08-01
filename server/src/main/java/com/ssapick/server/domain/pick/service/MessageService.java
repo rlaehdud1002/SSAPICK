48a36ae032be9ba3b823c7a5626f651c685e3b23
lij0825 <dldlswns890@gmail.com>
2024-08-01 21:44:34 +0900

    [FEAT] MessageData 보낸사람 받은사람 성별 추가

--- server/src/main/java/com/ssapick/server/domain/pick/service/MessageService.java
+++ server/src/main/java/com/ssapick/server/domain/pick/service/MessageService.java
@@ -22,97 +22,97 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MessageService {
-    private final MessageRepository messageRepository;
-    private final PickRepository pickRepository;
-    private final EntityManager em;
+	private final MessageRepository messageRepository;
+	private final PickRepository pickRepository;
+	private final EntityManager em;
 
-    /**
-     * 보낸 메시지 조회하기
-     * 보낸 메시지를 조회하고 익명으로 보내진 메시지는 익명으로 표시한다.
-     *
-     * @param user 로그인된 사용자
-     * @return {@link MessageData.Search} 보낸 메시지 리스트
-     */
-    public List<MessageData.Search> searchSendMessage(User user) {
-        return messageRepository.findSentMessageByUserId(user.getId()).stream()
-                .map((message) -> MessageData.Search.fromEntity(message, false))
-                .toList();
-    }
+	/**
+	 * 보낸 메시지 조회하기
+	 * 보낸 메시지를 조회하고 익명으로 보내진 메시지는 익명으로 표시한다.
+	 *
+	 * @param user 로그인된 사용자
+	 * @return {@link MessageData.Search} 보낸 메시지 리스트
+	 */
+	public List<MessageData.Search> searchSendMessage(User user) {
+		return messageRepository.findSentMessageByUserId(user.getId()).stream()
+			.map((message) -> MessageData.Search.fromEntity(message, false))
+			.toList();
+	}
 
-    /**
-     * 받은 메시지 조회하기
-     * 받은 메시지를 조회하고 익명으로 보내진 메시지는 익명으로 표시한다.
-     *
-     * @param user 로그인된 사용자
-     * @return {@link MessageData.Search} 받은 메시지 리스트
-     */
-    public List<MessageData.Search> searchReceiveMessage(User user) {
-        return messageRepository.findReceivedMessageByUserId(user.getId()).stream()
-                .map((message) -> MessageData.Search.fromEntity(message, true))
-                .toList();
-    }
+	/**
+	 * 받은 메시지 조회하기
+	 * 받은 메시지를 조회하고 익명으로 보내진 메시지는 익명으로 표시한다.
+	 *
+	 * @param user 로그인된 사용자
+	 * @return {@link MessageData.Search} 받은 메시지 리스트
+	 */
+	public List<MessageData.Search> searchReceiveMessage(User user) {
+		return messageRepository.findReceivedMessageByUserId(user.getId()).stream()
+			.map((message) -> MessageData.Search.fromEntity(message, true))
+			.toList();
+	}
 
-    /**
-     * 메시지 보내기
-     * 메시지를 보내고 픽의 메시지 전송 여부를 true 로 변경한다.
-     *
-     * @param sender 보내는 사람
-     * @param create {@link MessageData.Create} 메시지 생성 정보
-     */
-    @Transactional
-    public void createMessage(User sender, MessageData.Create create) {
-        Pick pick = pickRepository.findById(create.getPickId()).orElseThrow(
-            () ->new BaseException(ErrorCode.NOT_FOUND_PICK)
-        );
+	/**
+	 * 메시지 보내기
+	 * 메시지를 보내고 픽의 메시지 전송 여부를 true 로 변경한다.
+	 *
+	 * @param sender 보내는 사람
+	 * @param create {@link MessageData.Create} 메시지 생성 정보
+	 */
+	@Transactional
+	public void createMessage(User sender, MessageData.Create create) {
+		Pick pick = pickRepository.findById(create.getPickId()).orElseThrow(
+			() -> new BaseException(ErrorCode.NOT_FOUND_PICK)
+		);
 
-        if (pick.isMessageSend()) {
-            throw new BaseException(ErrorCode.ALREADY_SEND_MESSAGE);
-        }
+		if (pick.isMessageSend()) {
+			throw new BaseException(ErrorCode.ALREADY_SEND_MESSAGE);
+		}
 		pick.send();
 
-        // FIXME: 유저에 대한 정보를 entity manager 를 사용해서 가져오는데 이게 올바른 방법일까?
-        User receiver = em.getReference(User.class, create.getReceiverId());
+		// FIXME: 유저에 대한 정보를 entity manager 를 사용해서 가져오는데 이게 올바른 방법일까?
+		User receiver = em.getReference(User.class, create.getReceiverId());
 
-        messageRepository.save(Message.createMessage(sender, receiver, pick, create.getContent()));
-    }
+		messageRepository.save(Message.createMessage(sender, receiver, pick, create.getContent()));
+	}
 
-    /**
-     * 받은 메시지 삭제하기
-     * 로그인된 사용자를 받아 로그인 된 사용자가 받은 메시지라면 메시지를 삭제한다.
-     *
-     * @param receiver  메시지를 받은 사람
-     * @param messageId 받은 메시지 ID
-     */
-    @Transactional
-    public void deleteReceiveMessage(User receiver, Long messageId) {
-        Message message = messageRepository.findById(messageId).orElseThrow(
-            () -> new BaseException(ErrorCode.NOT_FOUND_MESSAGE)
-        );
+	/**
+	 * 받은 메시지 삭제하기
+	 * 로그인된 사용자를 받아 로그인 된 사용자가 받은 메시지라면 메시지를 삭제한다.
+	 *
+	 * @param receiver  메시지를 받은 사람
+	 * @param messageId 받은 메시지 ID
+	 */
+	@Transactional
+	public void deleteReceiveMessage(User receiver, Long messageId) {
+		Message message = messageRepository.findById(messageId).orElseThrow(
+			() -> new BaseException(ErrorCode.NOT_FOUND_MESSAGE)
+		);
 
-        if (!Objects.equals(message.getReceiver().getId(), receiver.getId())) {
-            throw new BaseException(ErrorCode.FORBIDDEN);
-        }
+		if (!Objects.equals(message.getReceiver().getId(), receiver.getId())) {
+			throw new BaseException(ErrorCode.FORBIDDEN);
+		}
 
-        message.deleteMessageOfReceiver();
-    }
+		message.deleteMessageOfReceiver();
+	}
 
-    /**
-     * 보낸 메시지 삭제하기
-     * 로그인된 사용자를 받아 로그인 된 사용자가 보낸 메시지라면 메시지를 삭제한다.
-     *
-     * @param sender    메시지를 보낸 사람
-     * @param messageId 보낸 메시지 ID
-     */
-    @Transactional
-    public void deleteSendMessage(User sender, Long messageId) {
-        Message message = messageRepository.findById(messageId).orElseThrow(
-            () -> new BaseException(ErrorCode.NOT_FOUND_MESSAGE)
-        );
+	/**
+	 * 보낸 메시지 삭제하기
+	 * 로그인된 사용자를 받아 로그인 된 사용자가 보낸 메시지라면 메시지를 삭제한다.
+	 *
+	 * @param sender    메시지를 보낸 사람
+	 * @param messageId 보낸 메시지 ID
+	 */
+	@Transactional
+	public void deleteSendMessage(User sender, Long messageId) {
+		Message message = messageRepository.findById(messageId).orElseThrow(
+			() -> new BaseException(ErrorCode.NOT_FOUND_MESSAGE)
+		);
 
-        if (!Objects.equals(message.getSender().getId(), sender.getId())) {
-            throw new BaseException(ErrorCode.FORBIDDEN);
-        }
+		if (!Objects.equals(message.getSender().getId(), sender.getId())) {
+			throw new BaseException(ErrorCode.FORBIDDEN);
+		}
 
-        message.deleteMessageOfSender();
-    }
+		message.deleteMessageOfSender();
+	}
 }
