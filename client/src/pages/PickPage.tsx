f4de52756c5fe8a01e2e170642b6a7b21f17cd2e
김도영 <rlaehdud1002@naver.com>
2024-08-01 22:51:20 +0900

    [FEAT] 질문 조회 axios

--- client/src/pages/PickPage.tsx
+++ client/src/pages/PickPage.tsx
@@ -1,38 +1,46 @@
 import Question from 'components/PickPage/QuestionBox';
 import Choice from 'components/PickPage/ChoiceBox';
 import ShuffleIcon from 'icons/ShuffleIcon';
+import { useQuery } from '@tanstack/react-query';
+import { IQuestion } from 'atoms/Pick.type';
+import { getQuestion } from 'api/questionApi';
 // import CoolTime from 'components/PickPage/CoolTime';
 // import PickComplete from "components/PickPage/PickComplete";
 
-import { useRecoilValue } from 'recoil';
-import { questionState } from 'atoms/PickAtoms';
-
 const Pick = () => {
-  const questions = useRecoilValue(questionState);
+  const { data: questions, isLoading } = useQuery<IQuestion[]>({
+    queryKey: ['questions'],
+    queryFn: getQuestion,
+  });
+
+  // ! FIXED : questions가 빈 배열은 ok인데 왜 undefined일까? 
+  console.log('questions', questions, isLoading);
+
   return (
     <div className="relative">
       {/* <PickComplete /> */}
       {/* <CoolTime /> */}
-      {questions.map((question, index) => {
-        return (
-          <div>
-            <Question questionInfo={question} />
-            <div className="m-7">
-              <div className="flex flex-row justify-end">
-                <ShuffleIcon className="cursor-pointer" />
-              </div>
-              <div className="flex flex-row justify-center">
-                <Choice username="민준수" gen="M" />
-                <Choice username="이호영" gen="F" />
-              </div>
-              <div className="flex flex-row justify-center">
-                <Choice username="이인준" gen="M" />
-                <Choice username="황성민" gen="M" />
+      {questions &&
+        questions.map((question, index) => {
+          return (
+            <div>
+              <Question question={question} />
+              <div className="m-7">
+                <div className="flex flex-row justify-end">
+                  <ShuffleIcon className="cursor-pointer" />
+                </div>
+                <div className="flex flex-row justify-center">
+                  <Choice username="민준수" gen="M" />
+                  <Choice username="이호영" gen="F" />
+                </div>
+                <div className="flex flex-row justify-center">
+                  <Choice username="이인준" gen="M" />
+                  <Choice username="황성민" gen="M" />
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 };
