7b4eacf0ff5bd2c2d12fdd87c4e6ffc7b552dc71
sunsuking <sunsuking@gmail.com>
2024-08-02 10:18:12 +0900

    [TEST] 레포지토리 테스트 코드 작성 완료

---
+++ server/src/test/java/com/ssapick/server/core/container/TestDatabaseContainer.java
@@ -0,0 +1,36 @@
+package com.ssapick.server.core.container;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@PropertySource("classpath:application-test.properties")
+public class TestDatabaseContainer {
+    private static final String DATABASE_NAME = "ssapick-test-db";
+    private static final String USERNAME = "ssapick-test-user";
+    private static final String PASSWORD = "ssapick-test-password";
+    private static final PostgreSQLContainer<?> postgreSQLContainer;
+
+    static {
+        postgreSQLContainer = new PostgreSQLContainer("postgres:16.3-alpine")
+                .withDatabaseName(DATABASE_NAME)
+                .withUsername(USERNAME)
+                .withPassword(PASSWORD);
+
+        postgreSQLContainer.start();
+
+    }
+
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        System.out.println(postgreSQLContainer.getJdbcUrl());
+        dynamicPropertyRegistry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.name", () -> DATABASE_NAME);
+        dynamicPropertyRegistry.add("spring.datasource.username", () -> USERNAME);
+        dynamicPropertyRegistry.add("spring.datasource.password", () -> PASSWORD);
+        dynamicPropertyRegistry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+    }
+}
