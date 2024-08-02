98ac84e02af17f43dc7af4f79290c19ffba80fee
김도영 <rlaehdud1002@naver.com>
2024-08-02 11:46:08 +0900

    [FEAT] route 정리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,18 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import CommonRoute from "components/Routes/CommonRoute";
-import LoginRoute from "components/Routes/LoginRoute";
-import ProfileRoute from "components/Routes/ProfileRoute";
-import { useLocation } from "react-router-dom";
-import { RecoilRoot } from "recoil";
-import RecoilNexus from "recoil-nexus";
-import Footer from "./components/common/Footer";
-import Header from "./components/common/Header";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import CommonRoute from 'components/Routes/CommonRoute';
+import LoginRoute from 'components/Routes/LoginRoute';
+import ProfileRoute from 'components/Routes/ProfileRoute';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import RecoilNexus from 'recoil-nexus';
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
+import NotFoundPage from 'pages/NotFoundPage';
+
+// ! FIXED NotFoundPage 렌더링 이상하게 됨
 
 function App() {
-  const location = useLocation().pathname.split("/")[1];
+  const location = useLocation().pathname.split('/')[1];
   const queryClient = new QueryClient();
   return (
     <RecoilRoot>
@@ -17,13 +20,17 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <div className="flex flex-col relative">
           <div className="flex flex-col max-h-screen">
-            {location !== "" && location !== "splash" && <Header />}
+            {location !== '' && location !== 'splash' && <Header />}
             <div className="flex-grow">
-              <CommonRoute />
-              <LoginRoute />
-              <ProfileRoute />
+              <Routes>
+                <Route path="/*" element={<CommonRoute />} />
+                <Route path="/login/*" element={<LoginRoute />} />
+                <Route path="/profile/*" element={<ProfileRoute />} />
+                {/* 잘못된 접근일 때
+                <Route path="*" element={<NotFoundPage />} /> */}
+              </Routes>
               <div className="flex flex-col max-h-screen">
-                {location !== "" && location !== "splash" && <Footer />}
+                {location !== '' && location !== 'splash' && <Footer />}
               </div>
             </div>
           </div>
--- client/src/components/Routes/CommonRoute.tsx
+++ client/src/components/Routes/CommonRoute.tsx
@@ -1,22 +1,22 @@
-import Received from "components/MessagePage/Received";
-import Send from "components/MessagePage/Send";
-import Alarm from "pages/AlarmPage";
-import Home from "pages/HomePage";
-import Login from "pages/LoginPage";
-import Message from "pages/MessagePage";
-import Pick from "pages/PickPage";
-import Profile from "pages/ProfilePage";
-import Ranking from "pages/RankingPage";
-import Splash from "pages/SplashPage";
-import { Route, Routes } from "react-router-dom";
+import Received from 'components/MessagePage/Received';
+import Send from 'components/MessagePage/Send';
+import Alarm from 'pages/AlarmPage';
+import Home from 'pages/HomePage';
+import Login from 'pages/LoginPage';
+import Message from 'pages/MessagePage';
+import NotFoundPage from 'pages/NotFoundPage';
+import Pick from 'pages/PickPage';
+import Ranking from 'pages/RankingPage';
+import Splash from 'pages/SplashPage';
+import { Route, Routes } from 'react-router-dom';
 
 const CommonRoute = () => {
   return (
     <Routes>
+      <Route path="/" element={<Login />} />
       <Route path="/splash" element={<Splash />} />
       <Route path="/alarm" element={<Alarm />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/" element={<Login />} />
       <Route path="/ranking" element={<Ranking />} />
       <Route path="/pick" element={<Pick />} />
       <Route path="/message" element={<Message />}>
@@ -24,7 +24,6 @@ const CommonRoute = () => {
         <Route path="received" element={<Received />} />
         <Route path="send" element={<Send />} />
       </Route>
-      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
