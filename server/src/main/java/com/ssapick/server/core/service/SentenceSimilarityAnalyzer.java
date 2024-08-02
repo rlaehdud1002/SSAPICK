8091beb86202fa2def46a0ef0c1f85de5def7841
Seongmin Hwang <qq221qq@naver.com>
2024-08-02 11:07:46 +0900

    [FIX] fastapi properties 변경

--- server/src/main/java/com/ssapick/server/core/service/SentenceSimilarityAnalyzer.java
+++ server/src/main/java/com/ssapick/server/core/service/SentenceSimilarityAnalyzer.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "fastApi", url = "${spring.localhost}")
+@FeignClient(name = "fastApi", url = "${fast_api.localhost}")
 public interface SentenceSimilarityAnalyzer {
 	@GetMapping("/v1/similarity")
 	String analyzeSentenceSimilarity(@RequestParam("text") String text);
