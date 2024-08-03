b6dec000b983b6e30c9ac61a8aaa605f2b7fd46d
lij0825 <dldlswns890@gmail.com>
2024-08-03 20:31:30 +0900

    [FIX] Authenticated 어노테이션 추가

--- server/src/main/java/com/ssapick/server/domain/user/controller/AlarmController.java
+++ server/src/main/java/com/ssapick/server/domain/user/controller/AlarmController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssapick.server.core.annotation.Authenticated;
 import com.ssapick.server.core.annotation.CurrentUser;
 import com.ssapick.server.core.response.SuccessResponse;
 import com.ssapick.server.domain.user.dto.AlarmData;
@@ -24,6 +25,7 @@ public class AlarmController {
 	 * 알람 조회 API
 	 * @return {@link AlarmData.Response} 조회된 알람 리스트
 	 */
+	@Authenticated
 	@GetMapping(value = "")
 	public SuccessResponse<AlarmData.Response> getAlarm(@CurrentUser User user) {
 		return SuccessResponse.of(alarmService.getAlarm(user.getId()));
@@ -34,6 +36,7 @@ public class AlarmController {
 	 * @param update 업데이트할 알람 정보
 	 * {@link AlarmData.Update}
 	 */
+	@Authenticated
 	@PostMapping(value = "")
 	public SuccessResponse<Void> updateAlarm(@CurrentUser User user, @RequestBody AlarmData.Update update) {
 		alarmService.updateAlarm(user.getId(), update);
