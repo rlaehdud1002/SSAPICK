5f40ee63abdf148032c7d7e7b20726ca1121de6d
Seongmin Hwang <qq221qq@naver.com>
2024-08-03 15:00:37 +0900

    [FEAT] Question Repository Test

--- server/src/main/java/com/ssapick/server/domain/question/repository/QuestionBanRepository.java
+++ server/src/main/java/com/ssapick/server/domain/question/repository/QuestionBanRepository.java
@@ -14,6 +14,6 @@ public interface QuestionBanRepository extends JpaRepository<QuestionBan, Long> 
 	@Query("SELECT qb FROM QuestionBan qb WHERE qb.user.id = :userId AND qb.question.id = :questionId")
 	Optional<QuestionBan> findBanByUserIdAndQuestionId(@Param("userId") Long userId, @Param("questionId") Long questionId);
 
-	@Query("SELECT qb FROM QuestionBan qb WHERE qb.user.id = :userId")
+	@Query("SELECT qb.question FROM QuestionBan qb WHERE qb.user.id = :userId")
 	List<Question> findQBanByUserId(@Param("userId") Long userId);
 }
