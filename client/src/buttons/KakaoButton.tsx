75a2e0cf199ef653a715d06c75fad429dc89d173
sunsuking <sunsuking@gmail.com>
2024-08-03 16:13:12 +0900

    [HOTFIX] 로그인 바로 리다이렉트 안되는 문제 해결

--- client/src/buttons/KakaoButton.tsx
+++ client/src/buttons/KakaoButton.tsx
@@ -6,13 +6,15 @@ import { Link } from "react-router-dom";
 const KakaoButton = () => {
 
   return (
-      <Link
-        to={KAKAO_AUTH_URL}
+      <button
+        onClick={() => {
+          window.location.href = KAKAO_AUTH_URL;
+        }}
         type="button"
         className="flex my-2 items-center justify-center backGround-kakao w-72 h-14 rounded-lg">
         <img className="w-8 h-8 mr-4" src="icons/Kakao.png" alt="카카오로 시작하기" />
         카카오로 시작하기
-      </Link>
+      </button>
   );
 };
 
