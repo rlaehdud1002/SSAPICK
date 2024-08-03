4a8a9b5436abac908f8af20d2b6cca4815686a2c
JaewonChoi <wefhio1985@gmail.com>
2024-08-03 16:37:06 +0900

    [TEST] UserController 로그인한 사용자 정보 조회 테스트 작성

--- server/src/main/java/com/ssapick/server/domain/user/controller/UserController.java
+++ server/src/main/java/com/ssapick/server/domain/user/controller/UserController.java
@@ -1,21 +1,20 @@
 package com.ssapick.server.domain.user.controller;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.ssapick.server.core.annotation.Authenticated;
 import com.ssapick.server.core.annotation.CurrentUser;
-import com.ssapick.server.core.exception.BaseException;
-import com.ssapick.server.core.exception.ErrorCode;
 import com.ssapick.server.core.response.SuccessResponse;
-import com.ssapick.server.core.service.S3Service;
 import com.ssapick.server.domain.user.dto.ProfileData;
 import com.ssapick.server.domain.user.dto.UserData;
 import com.ssapick.server.domain.user.entity.User;
 import com.ssapick.server.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -24,8 +23,6 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class UserController {
 	private final UserService userService;
-	private final S3Service s3Service;
-	private final AmazonS3 s3;
 
 	/**
 	 * 로그인한 사용자 정보 조회 API
@@ -35,24 +32,17 @@ public class UserController {
 	@Authenticated
 	@GetMapping(value = "/me")
 	public SuccessResponse<UserData.UserInfo> findLoggedInUser(@CurrentUser User user) {
-		System.out.println(user);
-		return SuccessResponse.of(userService.getUserInfo(user));
+      return SuccessResponse.of(userService.getUserInfo(user));
 	}
 
 	@PatchMapping(value = "", consumes = "multipart/form-data")
 	public SuccessResponse<Void> updateProfile(
 		@CurrentUser User user,
 		@RequestPart(value = "profileImage", required = false) MultipartFile profileImage,
-		@Validated @RequestPart UserData.Update update,
-		Errors errors
+		@Valid @RequestPart UserData.Update update
 	) {
-		if (errors.hasErrors()) {
-			log.error("사용자 수정 입력 값 에러 발생: {}", errors);
-			throw new BaseException(ErrorCode.INVALID_INPUT_VALUE);
-		}
-
 		userService.updateUser(user.getId(), update, profileImage);
-		return SuccessResponse.of(null);
+		return SuccessResponse.empty();
 	}
 
 }
