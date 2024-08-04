d148a2878a8c5979a1b3a011cf6e2b2ddb2e5f73
sunsuking <sunsuking@gmail.com>
2024-08-04 17:37:40 +0900

    [HOTFIX] CORS 문제 해결

--- server/src/main/java/com/ssapick/server/core/configuration/SecurityConfig.java
+++ server/src/main/java/com/ssapick/server/core/configuration/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
         return httpSecurity
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                    configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://www.ssapick.kro.kr"));
                     configuration.setAllowedMethods(Collections.singletonList("*"));
                     configuration.setAllowCredentials(true);
                     configuration.setAllowedHeaders(Collections.singletonList("*"));
