d79b61f1352e71fff1543bd0b5c6271200ccaaf5
INJUN <dldlswns890@naver.com>
2024-08-07 14:27:53 +0900

    [FEAT] 찾을 수 없는 알람 에러 코드 추가

--- server/src/main/java/com/ssapick/server/core/exception/ErrorCode.java
+++ server/src/main/java/com/ssapick/server/core/exception/ErrorCode.java
@@ -12,19 +12,19 @@ public enum ErrorCode {
 	SERVER_ERROR(1000, HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러가 발생하였습니다."),
 
 	// Authentication, Authorization
-    UNAUTHORIZED(2000, HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
-    FORBIDDEN(2001, HttpStatus.FORBIDDEN, "권한이 없습니다."),
-    INVALID_TOKEN(2002, HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    EXPIRED_TOKEN(2003, HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
-    INVALID_REFRESH_TOKEN(2004, HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
-    EXPIRED_REFRESH_TOKEN(2005, HttpStatus.UNAUTHORIZED, "만료된 리프레시 토큰입니다."),
-    ALREADY_AUTHORIZED(2006, HttpStatus.BAD_REQUEST, "이미 인증된 사용자입니다."),
-    NOT_FOUND_USER(2007, HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    INVALID_ACCESS_TOKEN(2008, HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다."),
-    INVALID_MATTERMOST_INFO(2009, HttpStatus.UNAUTHORIZED, "메타모스트 정보가 일치하지 않습니다."),
-    ACCESS_DENIED(2010, HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
-    ALREADY_FOLLOWED_USER(2011, HttpStatus.BAD_REQUEST, "이미 팔로우한 사용자입니다."),
-    NOT_FOLLOWED_USER(2012, HttpStatus.BAD_REQUEST, "팔로우 되지 않은 사용자입니다."),
+	UNAUTHORIZED(2000, HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+	FORBIDDEN(2001, HttpStatus.FORBIDDEN, "권한이 없습니다."),
+	INVALID_TOKEN(2002, HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+	EXPIRED_TOKEN(2003, HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+	INVALID_REFRESH_TOKEN(2004, HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
+	EXPIRED_REFRESH_TOKEN(2005, HttpStatus.UNAUTHORIZED, "만료된 리프레시 토큰입니다."),
+	ALREADY_AUTHORIZED(2006, HttpStatus.BAD_REQUEST, "이미 인증된 사용자입니다."),
+	NOT_FOUND_USER(2007, HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+	INVALID_ACCESS_TOKEN(2008, HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다."),
+	INVALID_MATTERMOST_INFO(2009, HttpStatus.UNAUTHORIZED, "메타모스트 정보가 일치하지 않습니다."),
+	ACCESS_DENIED(2010, HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+	ALREADY_FOLLOWED_USER(2011, HttpStatus.BAD_REQUEST, "이미 팔로우한 사용자입니다."),
+	NOT_FOLLOWED_USER(2012, HttpStatus.BAD_REQUEST, "팔로우 되지 않은 사용자입니다."),
 	FCM_TOKEN_NOT_FOUND(2013, HttpStatus.NOT_FOUND, "FCM 토큰을 찾을 수 없습니다."),
 
 	// Invalid Value
@@ -41,7 +41,6 @@ public enum ErrorCode {
 	NOT_FOUND_QUESTION(5004, HttpStatus.NOT_FOUND, "질문을 찾을 수 없습니다."),
 	EXIST_QUESTION(5005, HttpStatus.BAD_REQUEST, "이미 질문이 존재합니다."),
 	NOT_FOUND_QUESTION_BAN(5006, HttpStatus.NOT_FOUND, "질문 차단을 찾을 수 없습니다."),
-
 
 	//Pick
 	NOT_FOUND_PICK(6001, HttpStatus.NOT_FOUND, "픽을 찾을 수 없습니다."),
@@ -67,8 +66,10 @@ public enum ErrorCode {
 
 	//UserBan
 	NOT_FOUND_USER_BAN(11000, HttpStatus.NOT_FOUND, "사용자 차단을 찾을 수 없습니다."),
-	ALREADY_BAN_USER(11001, HttpStatus.BAD_REQUEST, "이미 차단한 사용자입니다.");
+	ALREADY_BAN_USER(11001, HttpStatus.BAD_REQUEST, "이미 차단한 사용자입니다."),
 
+	// Notification
+	NOT_FOUND_NOTIFICATION(12000, HttpStatus.NOT_FOUND, "알림을 찾을 수 없습니다.");
 
 	private final int code;
 	private final HttpStatus status;
