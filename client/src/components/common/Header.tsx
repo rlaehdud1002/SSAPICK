d3b3f3c03c748a9c24665bf8d148b17648cea035
김도영 <rlaehdud1002@naver.com>
2024-08-07 23:32:59 +0900

    [FEAT] 쿨타임, 질문 신고 / 패스 모달

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,9 +1,9 @@
-import { useRecoilValue } from "recoil";
-import AlarmIcon from "../../icons/AlarmIcon";
-import CoinIcon from "../../icons/CoinIcon";
-import Logo from "../../icons/Logo";
-import { userCoinState } from "atoms/UserAtoms";
-import { Link } from "react-router-dom";
+import { useRecoilValue } from 'recoil';
+import AlarmIcon from '../../icons/AlarmIcon';
+import CoinIcon from '../../icons/CoinIcon';
+import Logo from '../../icons/Logo';
+import { userCoinState } from 'atoms/UserAtoms';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const coin = useRecoilValue(userCoinState);
