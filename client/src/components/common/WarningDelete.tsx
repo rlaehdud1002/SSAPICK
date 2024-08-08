bcc21b9877891f70bb53f00fa588f97818c0440c
김도영 <rlaehdud1002@naver.com>
2024-08-08 15:37:06 +0900

    [FIX] 보낸쪽지함 이름 수정

--- client/src/components/common/WarningDelete.tsx
+++ client/src/components/common/WarningDelete.tsx
@@ -2,8 +2,6 @@ import { Popover, PopoverContent, PopoverTrigger } from 'components/ui/popover';
 import PointIcon from 'icons/PointIcon';
 import { useLocation } from 'react-router-dom';
 import WarningDeleteModal from 'components/modals/WarningDeleteModal';
-import { useMutation } from '@tanstack/react-query';
-import { blockUser } from 'api/blockApi';
 import { IMessage } from 'atoms/Message.type';
 
 interface WarningDeleteProps {
@@ -13,15 +11,6 @@ interface WarningDeleteProps {
 const WarningDelete = ({ message }: WarningDeleteProps) => {
   const location = useLocation().pathname.split('/')[2];
 
-  // 유저 차단
-  const mutatiion = useMutation({
-    mutationKey: ['delete'],
-    mutationFn: blockUser,
-    onSuccess: () => {
-      console.log('유저 차단 성공');
-    },
-  });
-
   return (
     <Popover>
       <PopoverTrigger>
@@ -29,19 +18,12 @@ const WarningDelete = ({ message }: WarningDeleteProps) => {
       </PopoverTrigger>
       <PopoverContent className="mr-4 w-[102px] rounded-lg bg-[#E9F2FD] flex flex-col justify-center">
         {!(location === 'send') && (
-          <div
-            onClick={() => {
-              // messageId에서 userId로 입력
-              mutatiion.mutate(1);
-            }}
-          >
-            <WarningDeleteModal
-              messageId={message.id}
-              title="신고"
-              message={message.content}
-              location={location}
-            />
-          </div>
+          <WarningDeleteModal
+            messageId={message.id}
+            title="신고"
+            message={message.content}
+            location={location}
+          />
         )}
         <WarningDeleteModal
           messageId={message.id}
