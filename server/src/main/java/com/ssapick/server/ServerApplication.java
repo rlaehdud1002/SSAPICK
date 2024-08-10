8534f09bd516c245ca5beaceafbe9a482ca1fb45
Seongmin Hwang <qq221qq@naver.com>
2024-08-10 15:52:35 +0900

    [FEAT] 시간설정 취소

--- server/src/main/java/com/ssapick/server/ServerApplication.java
+++ server/src/main/java/com/ssapick/server/ServerApplication.java
@@ -12,9 +12,4 @@ public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
-
-    @PostConstruct
-    void started(){
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
 }
