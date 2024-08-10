205476dc9443ee982b64e7b4ae4581ba4fccd357
Seongmin Hwang <qq221qq@naver.com>
2024-08-10 16:05:29 +0900

    [FEAT] 시간 테스트

--- server/src/main/java/com/ssapick/server/ServerApplication.java
+++ server/src/main/java/com/ssapick/server/ServerApplication.java
@@ -12,4 +12,9 @@ public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
+
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
