44ff00552a419431d139f3480b6ca1a8168bd980
JaewonChoi <wefhio1985@gmail.com>
2024-08-11 18:01:49 +0900

    [FEAT] 유저 벤 목록에서 프로필 이미지 보여주기

--- client/src/components/BlockPage/BlockFriend.tsx
+++ client/src/components/BlockPage/BlockFriend.tsx
@@ -13,7 +13,7 @@ const BlockFriend = () => {
         <div className="mb-20">
             {blocks?.length ? (
             blocks.map((block, index) => (
-                <BlockFriendContent key={index} userId={block.userId} campusName={block.campusName} campusSection={block.campusSection} name={block.nickname}/>
+                <BlockFriendContent key={index} userId={block.userId} campusName={block.campusName} campusSection={block.campusSection} name={block.nickname} profileImage={block.profileImage} />
             ))):(
                 <div className="flex justify-center">
                     차단된 친구가 없습니다.
