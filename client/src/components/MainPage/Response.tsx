bf24035e8f439506a48919e90a484092adc2df69
김도영 <rlaehdud1002@naver.com>
2024-08-11 23:29:32 +0900

    [FEAT] 아코디언 수정, 메시지 신고 -> 차단, 픽 페이지 / 출석 모달 수정

--- client/src/components/MainPage/Response.tsx
+++ client/src/components/MainPage/Response.tsx
@@ -18,10 +18,9 @@ import Loading from 'components/common/Loading';
 
 interface ResponseProps {
   picks: IPick[];
-  isLoading: boolean;
 }
 
-const Response = ({ picks, isLoading }: ResponseProps) => {
+const Response = ({ picks }: ResponseProps) => {
   const [updatedPicks, setUpdatedPicks] = useState<IPick[]>([]);
 
   const { data: pickco, isLoading: isLoadingPickco } = useQuery<IPickco>({
@@ -55,73 +54,79 @@ const Response = ({ picks, isLoading }: ResponseProps) => {
     e.stopPropagation();
   }, []);
 
-  if (isLoadingPickco) {
+  const handleMessageSent = useCallback((pickId: number) => {
+    setUpdatedPicks((prevPicks) =>
+      prevPicks.map((pick) =>
+        pick.id === pickId ? { ...pick, messageSend: true } : pick,
+      ),
+    );
+  }, []);
+
+  if (isLoadingPickco || !pickco) {
     return <Loading />;
   }
 
   return (
     <div>
-      {pickco &&
-        updatedPicks.map((pick) => (
-          <div key={pick.id} className="rounded-lg bg-white/50 p-4 mb-5">
-            <Accordion type="single" collapsible>
-              <AccordionItem value="item-1" className="border-none">
-                <AccordionTrigger
-                  className="p-0"
-                  onClick={handleAccordionClick}
-                >
-                  <div className="flex flex-col">
-                    <div className="flex flex-row">
-                      <div onClick={handleMaskClick}>
-                        <UserMaskIcon
-                          pickId={pick.id}
-                          alarm={pick.alarm}
-                          gen={pick.sender.gender}
-                          onAlarmUpdate={handleAlarmUpdate}
-                        />
-                      </div>
-                      <h3 className="mx-3 text-color-000855">
-                        11기 {pick.sender.campusSection}반
-                      </h3>
-                    </div>
-                  </div>
-                </AccordionTrigger>
-                <p className="text-center my-4">{pick.question.content}</p>
-                <AccordionContent>
-                  <div className="flex flex-row justify-center items-center">
-                    <div className="rounded-lg bg-white/50 p-3 mx-10 min-w-20 max-w-40 text-center">
-                      <HintModal
-                        title={
-                          pick.openedHints.length === 0
-                            ? '?'
-                            : pick.openedHints[0]
-                        }
+      {updatedPicks.map((pick, index) => (
+        <div key={pick.id} className="rounded-lg bg-white/50 p-4 mb-5">
+          <Accordion
+            type="single"
+            collapsible
+            defaultValue={
+              pick.openedHints.length !== 0 ? String(index) : undefined
+            }
+          >
+            <AccordionItem value={String(index)} className="border-none">
+              <AccordionTrigger className="p-0" onClick={handleAccordionClick}>
+                <div className="flex flex-col">
+                  <div className="flex flex-row">
+                    <div onClick={handleMaskClick}>
+                      <UserMaskIcon
                         pickId={pick.id}
-                        pickco={pickco.pickco}
+                        alarm={pick.alarm}
+                        gen={pick.sender.gender}
+                        onAlarmUpdate={handleAlarmUpdate}
                       />
                     </div>
-                    <div className="rounded-lg bg-white/50 p-3 mx-10 min-w-20 max-w-40 text-center">
-                      <HintModal
-                        title={
-                          pick.openedHints.length <= 1
-                            ? '?'
-                            : pick.openedHints[1]
-                        }
-                        pickId={pick.id}
-                        pickco={pickco.pickco}
-                      />
-                    </div>
+                    <h3 className="mx-3 text-color-000855">
+                      11기 {pick.sender.campusSection}반
+                    </h3>
                   </div>
-                  {!pick.messageSend && (
-                    <div className="float-end">
-                      <MessageModal pick={pick} pickco={pickco.pickco} />
-                    </div>
-                  )}
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </div>
-        ))}
+                </div>
+              </AccordionTrigger>
+              <p className="text-center my-4">{pick.question.content}</p>
+              <AccordionContent>
+                <div className="flex flex-row justify-center items-center">
+                  <HintModal
+                    title={
+                      pick.openedHints.length === 0 ? '?' : pick.openedHints[0]
+                    }
+                    pickId={pick.id}
+                    pickco={pickco.pickco}
+                  />
+                  <HintModal
+                    title={
+                      pick.openedHints.length <= 1 ? '?' : pick.openedHints[1]
+                    }
+                    pickId={pick.id}
+                    pickco={pickco.pickco}
+                  />
+                </div>
+                {!pick.messageSend && (
+                  <div className="float-end">
+                    <MessageModal
+                      pick={pick}
+                      pickco={pickco.pickco}
+                      onMessageSent={() => handleMessageSent(pick.id)}
+                    />
+                  </div>
+                )}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      ))}
       <div className="h-24" />
     </div>
   );
