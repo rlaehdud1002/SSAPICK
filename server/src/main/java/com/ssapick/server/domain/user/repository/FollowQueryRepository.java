3f9766dd674c51067a2a4817e5ef2ca2a6d7d07e
Seongmin Hwang <qq221qq@naver.com>
2024-08-12 00:21:24 +0900

    [FEAT] 친구 추천 기능 작성

--- server/src/main/java/com/ssapick/server/domain/user/repository/FollowQueryRepository.java
+++ server/src/main/java/com/ssapick/server/domain/user/repository/FollowQueryRepository.java
@@ -2,8 +2,10 @@ package com.ssapick.server.domain.user.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+
 import com.ssapick.server.domain.user.dto.ProfileData;
 
 public interface FollowQueryRepository {
-	List<ProfileData.Friend> findRecommendFriends(Long userId);
+	List<ProfileData.Friend> findRecommendFriends(Long userId, Pageable pageable);
 }
