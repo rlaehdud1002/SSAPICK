7afeb6f5b518c861754942b7da1756c6d5992728
JaewonChoi <wefhio1985@gmail.com>
2024-08-11 15:06:48 +0900

    [FEAT] 알림 설정한 질문 조회

--- client/src/components/QuestionListPage/QuestionInfo.tsx
+++ client/src/components/QuestionListPage/QuestionInfo.tsx
@@ -1,20 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
 import AlarmedQuestion from "./AlarmedQuestion";
 import QuestionInfoContent from "./RankedQuestion";
+import { IPick } from "atoms/Pick.type";
+import { getAlarmPick } from "api/pickApi";
 
 const QuestionInfo = () => {
+  const { data: alarmPick, isLoading } = useQuery<IPick>({
+    queryKey: ["pick"],
+    queryFn: () => getAlarmPick(),
+  })
+
+  console.log("alarmPick", alarmPick)
+
+
   return (
     <div className="ml-5">
       <QuestionInfoContent />
       {/* <Separator className="my-4 mx-4" />  */}
       <div className="bg-white h-px w-90 mx-1 mt-5"></div>
-      <div className="mt-5">
-        <span>알림 설정한 질문</span>
+      <span>알림 설정한 질문</span>
+      {alarmPick && (<div className="mt-5">
         <div className="mb-20">
-          {[0].map((index) => (
-            <AlarmedQuestion key={index} gender="여자" title="같이 밥먹고 싶은 사람?" />
-          ))}
+          <AlarmedQuestion gender={alarmPick?.sender.gender} title={alarmPick?.question.content} />
         </div>
-      </div>
+      </div>)}
     </div>
   );
 };
