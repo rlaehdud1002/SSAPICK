1719a361b3910e2aaa08d66ef3e4cd47ba1436a2
INJUN <dldlswns890@naver.com>
2024-08-12 14:03:37 +0900

    [FEAT] 픽코 로그 페이지 디자인

--- client/src/components/PickcologPage/PickcoLogList.tsx
+++ client/src/components/PickcologPage/PickcoLogList.tsx
@@ -25,37 +25,39 @@ const getTypeDetails = (type: string) => {
   switch (type) {
     case "SIGN_UP":
       return {
-        imageUrl: "",
+        imageUrl:
+          "https://d2yu3js5fxqm1g.cloudfront.net/category/%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85.png",
         title: "회원가입 보상",
       };
     case "ATTENDANCE":
       return {
-        imageUrl: "",
+        imageUrl: "https://d2yu3js5fxqm1g.cloudfront.net/category/%EC%B6%9C%EC%84%9D.png",
         title: "출석 보상",
       };
     case "HINT_OPEN":
       return {
-        imageUrl: "",
+        imageUrl: "https://d2yu3js5fxqm1g.cloudfront.net/category/%ED%9E%8C%ED%8A%B8.png",
         title: "힌트 열기",
       };
     case "PICK":
       return {
-        imageUrl: "",
+        imageUrl: "https://d2yu3js5fxqm1g.cloudfront.net/category/pick.png",
         title: "픽 보상",
       };
     case "MESSAGE":
       return {
-        imageUrl: "",
+        imageUrl: "https://d2yu3js5fxqm1g.cloudfront.net/category/%EC%AA%BD%EC%A7%80.png",
         title: "메시지",
       };
     case "QUESTION_CREATE":
       return {
-        imageUrl: "",
+        imageUrl:
+          "https://d2yu3js5fxqm1g.cloudfront.net/category/%EC%A7%88%EB%AC%B8+%EC%83%9D%EC%84%B1.png",
         title: "질문 생성 보상",
       };
     case "RE_ROLL":
       return {
-        imageUrl: "",
+        imageUrl: "https://d2yu3js5fxqm1g.cloudfront.net/category/%EC%B9%9C%EA%B5%AC2.png",
         title: "사용자 리롤",
       };
     default:
@@ -72,12 +74,12 @@ const PickcoLogList = ({ pickcolog }: IPickcologContent) => {
   const { imageUrl, title } = getTypeDetails(pickcoLogType);
 
   return (
-    <div className="bg-white/50 m-5 p-5 rounded-lg flex items-start space-x-4">
+    <div className="bg-white/50 my-3 py-5 px-2 rounded-lg flex items-start space-x-4">
       <img src={imageUrl} alt={title} className="w-12 h-12 object-cover" />
 
       <div className="flex flex-col justify-center flex-1">
         <div className="text-sapick font-semibold">{title}</div>
-        <div className="text-gray-500">{formatDate(createdAt)}</div>
+        <div className="text-gray-500 text-xs pt-2">{formatDate(createdAt)}</div>
       </div>
 
       <div className="flex flex-col space-y-1">
