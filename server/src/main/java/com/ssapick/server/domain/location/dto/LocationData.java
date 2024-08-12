3034c4c6995f1f0d47470989223e21e5ef0527e3
sunsuking <sunsuking@gmail.com>
2024-08-12 11:24:49 +0900

    [FEAT] NoARGs 추ã가

--- server/src/main/java/com/ssapick/server/domain/location/dto/LocationData.java
+++ server/src/main/java/com/ssapick/server/domain/location/dto/LocationData.java
@@ -2,10 +2,12 @@ package com.ssapick.server.domain.location.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class LocationData {
     @Data
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class Geo {
         private double latitude;
         private double longitude;
@@ -13,6 +15,7 @@ public class LocationData {
 
     @Data
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class Request {
         private String username;
         private Geo geo;
