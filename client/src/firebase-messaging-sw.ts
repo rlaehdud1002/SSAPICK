27b10bf9c49b6dce74caf543947b6e35615eb2e7
sunsuking <sunsuking@gmail.com>
2024-08-12 16:24:32 +0900

    [FEAT] firebase 연동 처리

--- client/src/firebase-messaging-sw.ts
+++ client/src/firebase-messaging-sw.ts
@@ -8,9 +8,8 @@ import { registerToken } from 'api/notificationApi';
 
 
 export function requestPermission(messaging: any) {
-  const accessToken = getRecoil(accessTokenState);
   const firebaseToken = getRecoil(firebaseTokenState);
-  if (accessToken !== null && firebaseToken !== null) return;
+  if (firebaseToken !== null) return;
 
   void Notification.requestPermission().then((permission) => {
     if (permission === 'granted') {
