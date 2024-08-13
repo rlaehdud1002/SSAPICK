9c552f5c921969fad7a8651e11dca8a70c8e593e
lij0825 <dldlswns890@gmail.com>
2024-08-13 20:30:12 +0900

    [FEAT] 픽 완료 이미지 추가

---
+++ client/src/icons/PickGuideIcon2.tsx
@@ -0,0 +1,10 @@
+interface PickAlarmIconProps {
+  width: number;
+  height: number;
+}
+
+const PickGuideIcon2 = ({ width, height }: PickAlarmIconProps) => {
+  return <img width={width} height={height} src="icons/PickGuide1.png" alt="Pick" />;
+};
+
+export default PickGuideIcon2;
