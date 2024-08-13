a0ad0be5307365c9500c09de06a9f9f87e237303
이호영 <hoo4053@naver.com>
2024-08-13 16:24:52 +0900

    [FEAT] shadcn carousel 추가

--- client/src/components/GuidePage/HomeGuide.tsx
+++ client/src/components/GuidePage/HomeGuide.tsx
@@ -1,5 +1,26 @@
+import { Carousel, 
+  CarouselContent, 
+  CarouselItem, 
+  CarouselNext, 
+  CarouselPrevious 
+} from "components/ui/carousel";
+
 const HomeGuide = () => {
-  return <div></div>;
+  return (
+    <div>
+      <span className="text-lg">메인 페이지</span>
+
+    <Carousel className="bg-white">
+      <CarouselContent>
+        <CarouselItem>first</CarouselItem>
+        <CarouselItem>second</CarouselItem>
+        <CarouselItem>third</CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+    </div>
+  )
 };
 
 export default HomeGuide;
