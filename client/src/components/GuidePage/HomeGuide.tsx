635269275d9e8381d1b43f910fd9293bea343991
이호영 <hoo4053@naver.com>
2024-08-13 18:00:14 +0900

    [FEAT] todo 가이드 페이지

--- client/src/components/GuidePage/HomeGuide.tsx
+++ client/src/components/GuidePage/HomeGuide.tsx
@@ -1,25 +1,41 @@
-import { Carousel, 
-  CarouselContent, 
-  CarouselItem, 
-  CarouselNext, 
-  CarouselPrevious 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
 } from "components/ui/carousel";
+import MainpageGuide from "icons/MainpageGuide";
+import MainpageModal from "icons/MainpageModal";
 
 const HomeGuide = () => {
   return (
-    <div>
-      <span className="text-lg">메인 페이지</span>
+    <div className="mt-10">
+      <div className="mb-5">
+        <span className="text-3xl">메인 페이지</span>
+        <p>
+          <br />코인을 사용하여 나를 픽한 친구의 힌트를 확인할 수 있어요.
+        </p>
+      </div>
+      <Carousel className="bg-white/50 rounded-lg h-[550px]">
+        <CarouselContent>
 
-    <Carousel className="bg-white">
-      <CarouselContent>
-        <CarouselItem>first</CarouselItem>
-        <CarouselItem>second</CarouselItem>
-        <CarouselItem>third</CarouselItem>
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-    </div>
+          <CarouselItem className="flex flex-col items-center justify-center">
+            <p>내가 받은 픽의 리스트를 확인할 수 있어요!</p>
+            <MainpageGuide />
+          </CarouselItem>
+
+          <CarouselItem>
+            <MainpageModal />
+          </CarouselItem>
+
+          <CarouselItem>third</CarouselItem>
+
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div >
   )
 };
 
