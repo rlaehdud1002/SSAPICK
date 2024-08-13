3d4a1bfc0243620cdee875d3c60f3495c1b47941
김도영 <rlaehdud1002@naver.com>
2024-08-13 22:23:26 +0900

    [FEAT] install guide page 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import CommonRoute from "components/Routes/CommonRoute";
-import ProfileRoute from "components/Routes/ProfileRoute";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import Footer from "./components/common/Footer";
-import Header from "./components/common/Header";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import CommonRoute from 'components/Routes/CommonRoute';
+import ProfileRoute from 'components/Routes/ProfileRoute';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { isValidateState, validState } from "atoms/ValidAtoms";
+import { isValidateState, validState } from 'atoms/ValidAtoms';
 
+<<<<<<< Updated upstream
 import { initializeApp } from "firebase/app";
 import NotFoundPage from "pages/NotFoundPage";
 import { useEffect } from "react";
@@ -19,11 +20,25 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import { messaging } from "firebase-messaging-sw";
 import { registerToken } from "api/notificationApi";
 import { setRecoil } from "recoil-nexus";
+=======
+import { initializeApp } from 'firebase/app';
+import NotFoundPage from 'pages/NotFoundPage';
+import { useEffect } from 'react';
+import { validCheck } from 'api/validApi';
+import {
+  accessTokenState,
+  isLoginState,
+  refreshRequestState,
+} from 'atoms/UserAtoms';
+import { refresh } from 'api/authApi';
+import { getMessaging, onMessage } from 'firebase/messaging';
+import { requestPermission } from 'firebase-messaging-sw';
+>>>>>>> Stashed changes
 
 
 
 function App() {
-  const location = useLocation().pathname.split("/")[1];
+  const location = useLocation().pathname.split('/')[1];
   const queryClient = new QueryClient();
 
   const setFirebaseToken = useSetRecoilState(firebaseTokenState);
@@ -52,6 +67,12 @@ function App() {
     
   }, [])
 
+<<<<<<< Updated upstream
+=======
+  onMessage(messaging, (payload) => {
+    console.log('Message received. ', payload);
+  });
+>>>>>>> Stashed changes
 
   const navigate = useNavigate();
   const isValid = useRecoilValue(isValidateState);
@@ -59,45 +80,50 @@ function App() {
   const isAuthenticated = useRecoilValue(isLoginState);
   const headerFooter = () => {
     return (
-      location !== "" && // 로그인 페이지
-      location !== "splash" && // 스플래시 페이지
-      location !== "mattermost" && // mm 인증 페이지
-      location !== "404" && // 404 페이지
-      location !== "infoinsert" // 추가 정보 입력 페이지
+      location !== '' && // 로그인 페이지
+      location !== 'splash' && // 스플래시 페이지
+      location !== 'mattermost' && // mm 인증 페이지
+      location !== '404' && // 404 페이지
+      location !== 'infoinsert' && // 추가 정보 입력 페이지
+      location !== 'install' // 설치 가이드 페이지
     );
   };
 
   useEffect(() => {
     const checkValidity = async () => {
       try {
-        if (location === "splash") {
+        if (location === 'splash' || location === 'install') {
           return;
         }
         if (isValid) return;
         const data = await validCheck();
         setValidState(data);
         if (data.lockedUser) {
-          navigate("/");
+          navigate('/');
           return;
         } else if (!data.mattermostConfirmed) {
-          navigate("/mattermost");
+          navigate('/mattermost');
           return;
         } else if (!data.validInfo) {
-          navigate("/infoinsert");
+          navigate('/infoinsert');
           return;
-        } else if (!data.lockedUser && data.mattermostConfirmed && data.validInfo) {
+        } else if (
+          !data.lockedUser &&
+          data.mattermostConfirmed &&
+          data.validInfo
+        ) {
           if (
-            location.includes("infoinsert") ||
-            location.includes("mattermost") ||
-            location.includes("")
+            location.includes('infoinsert') ||
+            location.includes('mattermost') ||
+            location.includes('')
           ) {
-            navigate("/home");
+            navigate('/home');
             return;
           }
         }
       } catch (error) {
-        console.error("유효성 검사 실패", error);
-        navigate("/");
+        console.error('유효성 검사 실패', error);
+        navigate('/');
       }
     };
     checkValidity();
@@ -108,7 +134,7 @@ function App() {
       <ReactQueryDevtools buttonPosition="top-left" initialIsOpen={false} />
       <div className="flex flex-col relative min-h-screen">
         {headerFooter() && <Header />}
-        <main className="flex-grow mb-[70px]">
+        <main className="flex-grow mb-[70px] relative">
           <Routes>
             <Route path="/*" element={<CommonRoute />} />
             <Route path="/profile/*" element={<ProfileRoute />} />
