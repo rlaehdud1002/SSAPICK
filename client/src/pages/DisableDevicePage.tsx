846f6baf9ed058782b855eeca8e3814c1400f018
INJUN <dldlswns890@naver.com>
2024-08-14 16:01:34 +0900

    [FEAT] 디바이스 검사

---
+++ client/src/pages/DisableDevicePage.tsx
@@ -0,0 +1,20 @@
+const DisableDevicePage = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <iframe
+          src="https://giphy.com/embed/10tIjpzIu8fe0"
+          width="100%"
+          height="30%"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+        <p className="text-3xl mt-4 noto-sans">죄송합니다</p>
+        <p className="text-3xl mt-4 noto-sans">현재 해상도는 현재 지원하지 않습니다</p>
+        <p className="text-3xl mt-4 noto-sans">ㅜㅜ</p>
+      </div>
+    </div>
+  );
+};
+
+export default DisableDevicePage;
